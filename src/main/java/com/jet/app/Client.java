404ada8b17a4ba17ecb4fdb3c3f3b6ca1f8ed55a
aa.korovushkin <eve-live@yandex.ru>
2017-06-23 18:20:55 +0300

    Chat

---
+++ src/main/java/com/jet/app/Client.java
@@ -0,0 +1,22 @@
+package com.jet.app;
+
+ class Client {
+
+    static void start(){
+        System.out.println("Welcome, Client!");
+    }
+
+    static void sendMessage(){
+        String sendMessageClient;
+
+    }
+
+    static void receiveMessage(){
+        String receiveMessageClient;
+    }
+
+    static void parametres(){
+        int param;
+
+    }
+}
