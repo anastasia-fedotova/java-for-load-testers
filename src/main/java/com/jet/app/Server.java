0fff67609b82e0133bd572afe8c08fe7e7a81648
aa.korovushkin <eve-live@yandex.ru>
2017-06-23 18:20:05 +0300

    test commit

---
+++ src/main/java/com/jet/app/Server.java
@@ -0,0 +1,28 @@
+package com.jet.app;
+
+ class Server {
+    static void start(){
+        System.out.println("Welcome, Server!");
+    }
+
+     static void sendMessage(){
+        String sendMessageServer;
+    }
+
+     static void receiveMessage(){
+        String receiveMessageServer;
+    }
+     static void historyMessage(){
+        String allMessage;
+    }
+
+    static void analiticsMessage(){
+        int messageCount;
+    }
+    static void logChat(){
+
+    }
+
+
+}
+
