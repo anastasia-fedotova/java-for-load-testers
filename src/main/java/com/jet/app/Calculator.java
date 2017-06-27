2caafa9ff28d7cfa45e2dda6b39f95216d37bc00
aa.korovushkin <eve-live@yandex.ru>
2017-06-27 10:31:00 +0300

    Chat commit

--- src/main/java/com/jet/app/Calculator.java
+++ src/main/java/com/jet/app/Calculator.java
@@ -1,24 +1,36 @@
 package com.jet.app;
 
 public class Calculator {
-    private static int sum;
-
+    static int sum;
+    public static void getSum() {
+        System.out.println(sum);
+    }
     public static int add(int a, int b) {
+        mSumma(a + b);
         return a + b;
     }
 
     public static int sub(int a, int b) {
+        mSumma(a - b);
         return a - b;
     }
 
     public static int div(int a, int b) {
+        mSumma(a / b);
         return a / b;
     }
 
-    public static int mSumma(int a) {
-        int msum = a + sum;
+    public static int mult(int a, int b){
+        mSumma(a * b);
+        return a * b;
+    }
+
+    public static int mSumma(int c) {
+        int msum = c + sum;
         sum = msum;
         return msum;
+        //System.out.println(sum);
+
     }
 
 
--- src/main/java/com/jet/app/CalculatorApp.java
+++ src/main/java/com/jet/app/CalculatorApp.java
@@ -3,17 +3,43 @@ package com.jet.app;
 
 public class CalculatorApp {
     public static void main(String[] args) {
+        calcAdd();
+        calcSub();
+        calcDiv();
+        calcMult();
 
-        System.out.println(Calculator.add(2, 3));
-        System.out.println(Calculator.sub(2, 1));
-        System.out.println(Calculator.mSumma(2));
-        System.out.println(Calculator.mSumma(2));
-        System.out.println(Calculator.mSumma(Calculator.add(2, 3)));
-        System.out.println(Calculator.mSumma(Calculator.sub(2, 1)));
-        System.out.println(Calculator.div(4, 2));
-        System.out.println(Calculator.mSumma(Calculator.add(3, 7)));
+        //System.out.println(Calculator.add(-1, 0));
+        //System.out.println(Calculator.sub(2, 1));
+        //System.out.println(Calculator.mSumma(2));
+        //System.out.println(Calculator.mSumma(2));
+        //System.out.println(Calculator.mSumma(Calculator.add(2, 3)));
+        //System.out.println(Calculator.mSumma(Calculator.sub(2, 1)));
+        //System.out.println(Calculator.div(4, 2));
+        //System.out.println(Calculator.mSumma(Calculator.add(3, 7)));
 
 
     }
 
+    private static void calcAdd() {
+        System.out.println(Calculator.add(-1, 0));
+        Calculator.getSum();
+        //System.out.println(Calculator.sum);
+    }
+
+    private static void calcSub() {
+        System.out.println(Calculator.sub(2, 2));
+        Calculator.getSum();
+        //System.out.println(Calculator.sum);
+
+    }
+    public static void calcDiv(){
+        System.out.println(Calculator.div(6, 3));
+        Calculator.getSum();
+        //System.out.println(Calculator.sum);
+    }
+    public static void calcMult(){
+        System.out.println(Calculator.mult(5, 7));
+        Calculator.getSum();
+        //System.out.println(Calculator.sum);
+    }
 }
