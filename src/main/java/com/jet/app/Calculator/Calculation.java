d447397c3594215752940b5866b3161b7e7cd381
ar.fedotova <fedotova.a.r@gmail.com>
2017-06-30 15:38:16 +0300

    Calculator Strings args

--- src/main/java/com/jet/app/Calculator/Calculation.java
+++ src/main/java/com/jet/app/Calculator/Calculation.java
@@ -1,35 +1,75 @@
+/**
+ * реализация функций калькулятора
+ */
+
 package com.jet.app.Calculator;
 
 class Calculation {
+    private static double mButton = 0;
+    private static double storeCalculation = 0;
 
-    private static int mButton = 0;
-    private static int storeCalculation = 0;
+    private static double checkRange(double a) {
+        if ((a > -10.0) & (a < 10.0)) {
+            return a;
+        } else {
+            System.out.println("Параметр выходит за границу, значение присвоено 10.0");
+            return 10.0;
+        }
+    }
 
-    static int add(int a, int b) {
+    private static double checkZeroDiv(double a) {
+        if (a == 0) {
+            System.out.println("Деление на 0 невозможно > a = infinity");
+            return Double.POSITIVE_INFINITY;
+        }
+        return a;
+    }
+
+    static double add(double a, double b) {
+        a = checkRange(a);
+        b = checkRange(b);
         mButton = a + b;
+        System.out.println("Сумма: " + mButton);
         return a + b;
     }
 
-    static int sub(int a, int b) {
+    static double sub(double a, double b) {
+        a = checkRange(a);
+        b = checkRange(b);
         mButton = a - b;
+        System.out.println("Вычитание: " + mButton);
         return a - b;
     }
 
-    static int mult(int a, int b) {
+    static double mul(double a, double b) {
+        a = checkRange(a);
+        b = checkRange(b);
         mButton = a * b;
+        System.out.println("Умножение: " + mButton);
         return a * b;
     }
 
-    static int div(int a, int b) {
+    static double div(double a, double b) {
+        a = checkRange(a);
+        b = checkRange(b);
+        b = checkZeroDiv(b);
         mButton = a / b;
+        System.out.println("Деление: " + mButton);
         return a / b;
+    }
+
+    static double abs(double module) {
+        if (module < 0) {
+            return -module;
+        }
+        return module;
     }
 
     static void setmButton() {
         storeCalculation = mButton;
     }
 
-    static int getmButton() {
+    static double getmButton() {
         return storeCalculation;
     }
 
--- src/main/java/com/jet/app/Calculator/CalculatorApp.java
+++ src/main/java/com/jet/app/Calculator/CalculatorApp.java
@@ -1,20 +1,60 @@
 package com.jet.app.Calculator;
 
 import static com.jet.app.Calculator.Calculation.*;
+import static java.lang.Integer.parseInt;
 
 public class CalculatorApp {
 
-
     public static void main(String[] args) {
 
-        System.out.println(add(1, -1));     // +
-        System.out.println(sub(0, 1));      // -
-        mPlusbutton();                      // m+
-        System.out.println(div(1, 1));      // /
-        System.out.println(mult(-1, -1));   // *
-        System.out.println(getmButton());   // m
-        setmButton();                       // m
-        System.out.println(getmButton());   // m
+/*      System.out.println(add(12, -1));
+        System.out.println(abs(-10));
+        System.out.println(sub(abs(0), 10.5));
+        mPlusbutton();
+        System.out.println(div(1, 0));
+        System.out.println(mul(-1, abs(-9)));
+        System.out.println(getmButton());
+        setmButton();
+        System.out.println(abs(getmButton()));
+        System.out.println(add(1.0, 1.2));*/
 
+        String command = "";
+        String arg1 = "";
+        String arg2 = "";
+        int position = 0;
+
+        for (String current : args) {
+            switch (position++) {
+                case 0:
+                    command = current;
+                    break;
+                case 1:
+                    arg1 = current;
+                    break;
+                case 2: {
+                    arg2 = current;
+                    doCalculate(command, parseInt(arg1), parseInt(arg2));
+                    position = 0;
+                }
+            }
+        }
+    }
+
+    private static void doCalculate(String command, int arg1, int arg2) {
+        System.out.println("Calculating: " + command + " with " + arg1 + ", " + arg2);
+        switch (command) {
+            case "add":
+                add(arg1, arg2);
+                break;
+            case "sub":
+                sub(arg1, arg2);
+                break;
+            case "mul":
+                mul(arg1, arg2);
+                break;
+            case "div":
+                div(arg1, arg2);
+                break;
+        }
     }
 }
