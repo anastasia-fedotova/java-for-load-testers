a96a580d0e92dce0a684e00b3258dcb042a5a389
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-07-01 11:05:29 +0300

    Split to array for StringBuilder.

--- src/main/java/com/acme/edu/ArraysDemo.java
+++ src/main/java/com/acme/edu/ArraysDemo.java
@@ -12,5 +12,44 @@ public class ArraysDemo {
         Integer i1 = 127; //+intern
         Integer i2 = 127;
         System.out.println(i1 == i2); //NNNOOOOOOOOO!!!!!! WTF?????
+
+        /*
+
+        1. java -cp .jar CalculatorRunner add 1 1. div 3.0 0
+        2. log() - prints:
+         5 add   5 = ?
+         3 div 0 = ?
+         ...
+        3*. String[] <- log()
+
+         */
+    }
+}
+
+class Calculator {
+    private static StringBuilder log = new StringBuilder();
+
+    public static int add(int a, int b) {
+        log
+            .append(a)
+            .append(" add ")
+            .append(b)
+            .append(" = ")
+            .append(a + b)
+            .append("\n");
+
+        return a + b;
+    }
+
+    public static String[] getLog() {
+        return log.toString().split("\n");
+    }
+}
+
+class CalculatorRunner {
+    public static void main(String... args) {
+        for (String arg : args) {
+        }
+
     }
 }
